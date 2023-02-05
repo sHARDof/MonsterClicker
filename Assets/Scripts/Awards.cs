774e70de919dfa419a3b98eaddd3c1f372973429
sHARDof2 <87265465+sHARDof2@users.noreply.github.com>
2023-02-05 10:38:13 +0300

    BugFix

--- Assets/Scripts/Awards.cs
+++ Assets/Scripts/Awards.cs
@@ -1,16 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Awards : MonoBehaviour
 {
     [SerializeField] private GameObject Panel_Awards;
-    [SerializeField] private SpriteRenderer Awards_Anim_Sprite;
     [SerializeField] private Animator Animator_Awards;
     [SerializeField] private GameObject Click_Award;
-    [SerializeField] private Sprite[] Awards_sprites = { Clicks_Sprite };
 
-    private static Sprite Clicks_Sprite;
     public static int Clicks = 0;
 
     private void Update()
@@ -26,11 +21,10 @@ public class Awards : MonoBehaviour
     {
         Panel_Awards.SetActive(false);
     }   
-    public void Assigned_Award()
+    public void Assigned_Award() 
     {
         if (Clicks == 10)
         {
-            Awards_Anim_Sprite.sprite = Awards_sprites[0];
             Animator_Awards.SetInteger("IndexAwards", 1);
             Click_Award.SetActive(true);
         }
