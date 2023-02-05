7cdd93f8cc1265654458bcfbcc43983f16fd4733
sHARDof2 <87265465+sHARDof2@users.noreply.github.com>
2023-02-05 14:12:58 +0300

    Update
    BugFix
    +New awards

--- Assets/Scripts/Awards.cs
+++ Assets/Scripts/Awards.cs
@@ -4,7 +4,9 @@ public class Awards : MonoBehaviour
 {
     [SerializeField] private GameObject Panel_Awards;
     [SerializeField] private Animator Animator_Awards;
-    [SerializeField] private GameObject Click_Award;
+    [SerializeField] private SpriteRenderer Awards_Sprite;
+    [SerializeField] private GameObject Click_Award1, Click_Award2, Click_Award3, Click_Award4;
+    [SerializeField] private Sprite Clicks1, Clicks2, Clicks3, Clicks4;
 
     public static int Clicks = 0;
 
@@ -25,8 +27,29 @@ public class Awards : MonoBehaviour
     {
         if (Clicks == 10)
         {
-            Animator_Awards.SetInteger("IndexAwards", 1);
-            Click_Award.SetActive(true);
+            Animator_Awards.SetTrigger("AnimActive");
+            Click_Award1.SetActive(true);
+            Awards_Sprite.sprite = Clicks1;
+        }
+        if (Clicks == 100)
+        {
+            Animator_Awards.SetTrigger("AnimActive");
+            Click_Award2.SetActive(true);
+            Awards_Sprite.sprite = Clicks2;
+        }
+        if (Clicks == 1000)
+        {
+            Animator_Awards.SetTrigger("AnimActive");
+            Click_Award3.SetActive(true);
+            Awards_Sprite.sprite = Clicks3;
+        }
+        if (Clicks == 10000)
+        {
+            Animator_Awards.SetTrigger("AnimActive");
+            Click_Award4.SetActive(true);
+            Awards_Sprite.sprite = Clicks4;
         }
     }
 }
+
+
